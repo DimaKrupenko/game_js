f3e040e9bcee3341b1b65b8b08e2cbeb2cfc1f95
DimaKrupenko <dimakrupenko77@gmail.com>
2023-06-13 15:07:47 +0300

    upd

--- player.js
+++ player.js
@@ -3,5 +3,24 @@ export class Player {
     this.x = x;
     this.y = y;
     this.context = context;
+
+    this.image = new Image();
+    this.image.src = './img/player.png';
+    this.imageWidth = 50;
+    this.imageHeight = 60;
+  }
+
+  drawImg() {
+    this.context.drawImage(
+      this.image,
+      0,
+      0,
+      this.imageWidth,
+      this.imageHeight,
+      this.x - this.imageWidth / 2,
+      this.y - this.imageHeight / 2,
+      this.imageWidth,
+      this.imageHeight
+    );
   }
 }
