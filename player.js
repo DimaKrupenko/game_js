d4aa1bf2d48db8d04669416866c4e2021bc847b5
DimaKrupenko <dimakrupenko77@gmail.com>
2023-06-13 17:40:32 +0300

    upd

--- player.js
+++ player.js
@@ -3,6 +3,15 @@ export class Player {
     this.x = x;
     this.y = y;
     this.context = context;
+    this.cursorPosition = {
+      x: 0,
+      y: 0,
+    };
+
+    document.addEventListener('mousemove', e => {
+      this.cursorPosition.x = e.clientX;
+      this.cursorPosition.y = e.clientY;
+    });
 
     this.image = new Image();
     this.image.src = './img/player.png';
@@ -22,5 +31,16 @@ export class Player {
       this.imageWidth,
       this.imageHeight
     );
+  }
+
+  draw() {
+    this.context.save();
+    let angle = Match.atan2(
+      this.cursorPosition.y - this.y,
+      this.cursorPosition.x - this.x
+    );
+    this.context.translate(this.x, this, y);
+    this.context.rotate(angle + Match.PI / 2);
+    this.context.translate(-this.x, -this, y);
   }
 }
